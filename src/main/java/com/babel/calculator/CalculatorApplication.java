4cc1e6de1e84b53d96002f4813c772e7b4fdd9a9
Jose Sampedro Cid <josescid@hotmail.com>
2023-02-02 13:08:38 +0100

    rename package

--- src/main/java/com/babel/calculator/CalculatorApplication.java
+++ src/main/java/com/babel/calculator/CalculatorApplication.java
@@ -1,4 +1,4 @@
-package com.babel.calculator.calculator;
+package com.babel.calculator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
